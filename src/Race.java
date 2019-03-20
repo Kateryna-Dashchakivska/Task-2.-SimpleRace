c7d4bdcc86a5c37b75c4e5fe521d46c631cfc612
Kateryna-Dashchakivska <40372399+Kateryna-Dashchakivska@users.noreply.github.com>
2019-03-20 12:36:08 +0200

    Adding my updated project

---
+++ src/Race.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Race {
+
+	static final int totalNumberOfCars = 10;
+	static final int totalWinners = 3;
+	static final int totalDuration = 20;
+	static int startDistance = 0;
+	static RaceCar[] cars = new RaceCar[totalNumberOfCars];
+
+	public static void startRace() {
+
+		for (int i = 0; i < totalNumberOfCars; i++) {
+			RaceCar carThread = new RaceCar();
+			carThread.setName("Car" + (i + 1));
+			carThread.setDuration(totalDuration);
+			carThread.setDistance(startDistance);
+			cars[i] = carThread;
+			carThread.start();
+		}
+
+		for(int i = 0; i < totalNumberOfCars; i++) {
+			try {
+				cars[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}	
+		}
+
+		sortCars(cars);
+		printWinners(totalWinners);
+	}
+
+	public static void printWinners(int totalWinners) {
+		System.out.println("\nThe race is FINISHED!!!\n"); 
+		System.out.println("Total time: " + totalDuration + "seconds"); 
+		System.out.println("\nTop " + totalWinners + " WINNERS:\n");
+
+		for (int i = 0; i < totalWinners; i++) {
+			System.out.println(cars[i].getName() + " has passed the distance: " + cars[i].getDistance() + "m!" + " It takes: " +(i + 1) + " place!");
+		}
+	}
+
+	public static RaceCar[] sortCars (RaceCar[] raceCars) {   //rename according to acs/desc
+		Arrays.sort(raceCars, new Comparator<RaceCar>() {
+			public int compare(RaceCar car1, RaceCar car2) {
+				if (car1.getDistance() > car2.getDistance())
+					return -1;
+				if (car1.getDistance() < car2.getDistance())
+					return 1;
+				return 0;
+			}
+		});
+		return raceCars;
+	}
+}
---
+++ src/RaceCar.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+
+public class RaceCar extends Thread {
+
+	private int duration;
+	private int speed;
+	private int time;
+	private int distance;
+
+	public void setDuration(int sec) {
+		this.duration = sec;
+	}
+	public int getDuration() {
+		return this.duration;
+	}
+	public void setSpeed(int s) {
+		this.speed = s;
+	}
+	public int getSpeed() {
+		return this.speed;
+	}
+	public void setTime(int t) {
+		this.time = t;
+	}
+	public int getTime() {
+		return this.time;
+	}
+	public void setDistance(int d) {
+		this.distance = d;
+	}
+	public int getDistance() {
+		return this.distance;
+	}
+
+	public int raceDistance() {
+		this.time = 0;
+		while (time < duration) {
+			this.time++;
+			Random random = new Random();
+			int minSpeed = 1;
+			this.speed = random.nextInt(minSpeed + 100);
+			this.distance = this.speed * this.time;
+			System.out.println(this.getName() + ":   " + "Speed: " + this.getSpeed() + "m/s" + "   Distance: " + this.getDistance() + "m");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}	
+		}
+		return this.distance;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(this.getName() + " is starting the race.......");
+		this.raceDistance();
+	}
+
+}
